7cec8c59417e54e2e3a3b9b61a90ad05d4a01f8b
Gregor Bückendorf <gregor.bueck@gmx.de>
2017-01-31 21:12:43 +0100

    Seperated the Jump and Maus layers. Also added a view more obscure characters to the Step layer.

--- keymap.c
+++ keymap.c
@@ -24,12 +24,14 @@ static uint16_t tap_timer;
 #define _RIFT 1
 #define _MODS 2
 #define _STEP 3
-#define _JUMP 4
-#define _WAKE 5
+#define _MAUS 4
+#define _JUMP 5
+#define _WAKE 6
 
 enum whitefox_keycodes {
 	RIFT = SAFE_RANGE,
 	STEP,
+	MAUS,
 	JUMP,
 	MCTL,
 	MGUI,
@@ -81,9 +83,9 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   STEP,            DE_A,    DE_S,    DE_D,    DE_F,    DE_G,    DE_H,    DE_J,    DE_K,    DE_L,    DE_SCLN, DE_QUOT, XXXXXXX, KC_ENT,   KC_PGUP,
 
-  RIFT,       XXXXXXX, DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,    DE_N,    DE_M,    DE_COMM, DE_DOT,  DE_SLSH, RIFT,          KC_UP,   KC_PGDN,
+  RIFT,       XXXXXXX, DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,    DE_N,    DE_M,    DE_COMM, DE_DOT,  DE_SLSH, JUMP,          KC_UP,   KC_PGDN,
 
-  MCTL,       MGUI,       MALT,                            KC_SPC,                          KC_APP,  JUMP,    XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT
+  MCTL,       MGUI,       MALT,                            KC_SPC,                          KC_APP,  MAUS,    XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 
@@ -140,13 +142,13 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Step Layer
  * ,---------------------------------------------------------------.
- * |RST| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|SCR|SCL|BRK|
+ * |  °| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|SCR|SCL|BRK|
  * |---------------------------------------------------------------|
- * |     |   |   |  €|   |   |   |  ü|   |  ö|   |   |   |     |INS|
+ * |     |   |   |  €|   |   |   |  ü|   |  ö|  §|   |   |     |INS|
  * |---------------------------------------------------------------|
  * |      |  ä|  ß|   |   |   |   |   |   |   |   |   |        |HOM|
  * |---------------------------------------------------------------|
- * |        |   |   |   |   |   |   |  µ|   |   |   |      |   |END|
+ * |        |  ¹|  ²|  ³|   |   |   |  µ|   |   |   |      |   |END|
  * |---------------------------------------------------------------|
  * |    |    |    |                       |    |    |  |   |   |   |
  * `---------------------------------------------------------------'
@@ -154,13 +156,38 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_STEP] = KEYMAP(
   DE_RING, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_SLCK, KC_PAUS,
 
-  XXXXXXX,     XXXXXXX, XXXXXXX, DE_EURO, XXXXXXX, XXXXXXX, XXXXXXX, DE_UE,   XXXXXXX, DE_OE,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_INS,
+  XXXXXXX,     XXXXXXX, XXXXXXX, DE_EURO, XXXXXXX, XXXXXXX, XXXXXXX, DE_UE,   XXXXXXX, DE_OE,   DE_PARA, XXXXXXX, XXXXXXX, XXXXXXX,      KC_INS,
 
   _______,         DE_AE,   DE_SS,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_HOME,
 
-  _______,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DE_MU,   XXXXXXX, XXXXXXX, XXXXXXX, _______,       XXXXXXX, KC_END,
+  _______,    DE_SQ1,  DE_SQ2,  DE_SQ3,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DE_MU,   XXXXXXX, XXXXXXX, XXXXXXX, _______,       XXXXXXX, KC_END,
 
   _______,    _______,    _______,                         XXXXXXX,                         XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+),
+
+/* Mouse Layer
+ * ,---------------------------------------------------------------.
+ * |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+ * |---------------------------------------------------------------|
+ * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |   |
+ * |---------------------------------------------------------------|
+ * |      |   |   |   |   |   |   |   |   |   |   |   |        |   |
+ * |---------------------------------------------------------------|
+ * |        |   |   |   |   |   |   |   |   |   |   |      |   |   |
+ * |---------------------------------------------------------------|
+ * |    |    |    |                       |    |    |  |   |   |   |
+ * `---------------------------------------------------------------'
+ */
+[_MAUS] = KEYMAP(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+
+  _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      KC_BTN3,
+
+  _______,         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, _______,  KC_WH_U,
+
+  _______,    XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BTN1,       KC_MS_U, KC_WH_D,
+
+  _______,    _______,    _______,                         _______,                         _______, _______, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R
 ),
 
 /* Jump Layer
@@ -179,13 +206,13 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_JUMP] = KEYMAP(
   RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, GB_PWR,
 
-  XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_BTN3,
+  XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX,
 
-  KC_CAPS,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN2,  KC_WH_U,
+  KC_CAPS,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,
 
-  XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN1,       KC_MS_U, KC_WH_D,
+  XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,       XXXXXXX, XXXXXXX,
 
-  XXXXXXX,    XXXXXXX,    XXXXXXX,                         XXXXXXX,                         XXXXXXX, _______, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R
+  XXXXXXX,    XXXXXXX,    XXXXXXX,                         XXXXXXX,                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 ),
 
 /* Wake Layer
@@ -234,19 +261,27 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			}
 			return false;
 			break;
-		case JUMP:
+		case MAUS:
 			if (record->event.pressed) {
-				if (IS_LAYER_ON(_JUMP)) {
-					layer_off(_JUMP);
+				if (IS_LAYER_ON(_MAUS)) {
+					layer_off(_MAUS);
 				} else { 
 					tap_timer = timer_read();
-					clear_mods();
-					layer_on(_JUMP);
+					layer_on(_MAUS);
 				}
 			} else {
-				if ( IS_LAYER_ON(_JUMP) && timer_elapsed(tap_timer) > 150 ) {
-					layer_off(_JUMP);
+				if ( IS_LAYER_ON(_MAUS) && timer_elapsed(tap_timer) > 150 ) {
+					layer_off(_MAUS);
 				}
+			}
+			return false;
+			break;
+		case JUMP:
+			if (record->event.pressed) {
+				clear_mods();
+				layer_on(_JUMP);
+			} else {
+				layer_off(_JUMP);
 			}
 			return false;
 			break;
